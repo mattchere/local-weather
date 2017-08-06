b2ead61dcbb2bcec89eb999ac0c37d5d63815d5a
infinitepure <matt.j.cheah@gmail.com>
2017-08-07 00:26:38 +1000

    Beautify files

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $.get("https://fcc-weather-api.glitch.me/api/current?", {
       lat: position.coords.latitude,
       lon: position.coords.longitude
-    }, function(weather) {
+    }, function (weather) {
       $(".degrees").html("<p>" + weather.main.temp + "</p>");
       $("#weather-icon").attr("alt", weather.weather[0].description);
       $("#weather-icon").attr("src", weather.weather[0].icon);
@@ -15,19 +15,18 @@ $(document).ready(function () {
     });
   };
 
-  $(".symbol").click(function() {
+  $(".symbol").click(function () {
     if ($(this).text().trim().indexOf("C") !== -1) {
       var farenheit = ctof(parseFloat($(".degrees p").text().trim()));
       setText(farenheit.toFixed(1), "&degF");
-    }
-    else {
+    } else {
       var celsius = ftoc(parseFloat($(".degrees p").text().trim()));
       setText(celsius.toFixed(1), "&degC");
     }
   });
 
   function setText(degrees, symbol) {
-    $(".degrees").html("<p>" + degrees +"</p>");
+    $(".degrees").html("<p>" + degrees + "</p>");
     $(".symbol").html(symbol);
   }
 
@@ -39,4 +38,3 @@ $(document).ready(function () {
     return ((far - 32) * 5) / 9;
   }
 });
-
